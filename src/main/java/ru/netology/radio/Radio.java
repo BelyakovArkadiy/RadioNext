cd027016d6d3e977ad978a53d3f4e74795483d13
BelyakovArkadiy <belyakovarkadiy@gmail.com>
2021-02-01 11:43:36 +0600

    update Radio.java , added Lombok

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+
 public class Radio {
 
     private int currentChannel;
@@ -16,50 +17,54 @@ public class Radio {
     private double minSound = 0.00;
     private double maxSound = 100.00;
 
-    public Radio(int currentChanel) {
-        if (currentChanel >= minChannel && currentChanel <= maxChannel) {
-            this.currentChannel = currentChanel;
+
+    public void setCurrentChannel() {
+        if (currentChannel >= minChannel && currentChannel <= maxChannel) {
+            this.currentChannel = currentChannel;
         }
     }
 
 
-
-    public Radio(int currentChannel, int minChannel) {
+    public void setCurrentChannelMinus() {
         if (currentChannel > minChannel) {
-            this.currentChannel = currentChannel - 1;
+            this.currentChannel--;
         } else if (currentChannel < minChannel) {
             this.currentChannel = maxChannel;
         }
     }
 
-    public Radio(int currentChannel, int minChannel, int maxChannel) {
+    public void setCurrentChannelPlus() {
         if (currentChannel < maxChannel) {
-            this.currentChannel = currentChannel + 1;
+            this.currentChannel++;
         } else if (currentChannel > maxChannel) {
             this.currentChannel = minChannel;
         }
-
     }
 
-    public Radio(double currentSound, double maxSound) {
+
+    public void setCurrentSoundNext() {
         if (currentSound < maxSound) {
-            this.currentSound = currentSound + 1;
+            this.currentSound++;
         } else if (currentSound == maxSound) {
             this.currentSound = maxSound;
         }
-
     }
 
-    public Radio(double currentSound, double minSound, double maxSound) {
+    public void setCurrentSoundPrev() {
         if (currentSound > minSound) {
-            this.currentSound = currentSound - 1;
+            this.currentSound--;
         } else if (currentSound == minSound) {
             this.currentSound = minSound;
         }
-
-
     }
 
+    public Radio(int currentChannel) {
+        this.currentChannel = currentChannel;
+    }
 
+    public Radio(double currentSound) {
+        this.currentSound = currentSound;
+    }
 }
+
 
--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -7,60 +7,70 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
 
-
     @Test
     void currentChanelConsole() {
         Radio radio = new Radio(10);
+        radio.setCurrentChannel();
         assertEquals(10,radio.getCurrentChannel());
     }
     @Test
     void currentChanelLessMin() {
-        Radio radio = new Radio(-1,0);
+        Radio radio = new Radio(-1);
+        radio.setCurrentChannelMinus();
         assertEquals(25,radio.getCurrentChannel());
     }
     @Test
     void currentChanelFrontMin() {
-        Radio radio = new Radio(1,0);
+        Radio radio = new Radio(1);
+        radio.setCurrentChannelMinus();
         assertEquals(0,radio.getCurrentChannel());
     }
     @Test
     void currentChanelMinus() {
-        Radio radio = new Radio(11,0);
+        Radio radio = new Radio(11);
+        radio.setCurrentChannelMinus();
         assertEquals(10,radio.getCurrentChannel());
     }
-     @Test
+    @Test
     void currentChanelPlus() {
-        Radio radio = new Radio(10,0,25);
+        Radio radio = new Radio(10);
+        radio.setCurrentChannelPlus();
         assertEquals(11,radio.getCurrentChannel());
     }
     @Test
     void currentChanelLessMax() {
-        Radio radio = new Radio(26,0,25);
+        Radio radio = new Radio(26);
+        radio.setCurrentChannelPlus();
         assertEquals(0,radio.getCurrentChannel());
     }
     @Test
     void currentChanelFrontMax() {
-        Radio radio = new Radio(24,0,25);
+        Radio radio = new Radio(24);
+        radio.setCurrentChannelPlus();
         assertEquals(25,radio.getCurrentChannel());
     }
     @Test
     void currentSoundPlus() {
-        Radio radio = new Radio(19.00,100.00);
+        Radio radio = new Radio(19.00);
+        radio.setCurrentSoundNext();
         assertEquals(20,radio.getCurrentSound());
     }
     @Test
     void currentSoundMaxPlus() {
-        Radio radio = new Radio(100.00,100.00);
+        Radio radio = new Radio(100.00);
+        radio.setCurrentSoundNext();
         assertEquals(100,radio.getCurrentSound());
     }
     @Test
     void currentSoundMinus() {
-        Radio radio = new Radio(50.00,0.00,100);
+        Radio radio = new Radio(50.00);
+        radio.setCurrentSoundPrev();
         assertEquals(49,radio.getCurrentSound());
     }
     @Test
     void currentSoundMinMinus() {
-        Radio radio = new Radio(0.00,0.00,100);
+        Radio radio = new Radio(0.00);
+        radio.setCurrentSoundPrev();
         assertEquals(0,radio.getCurrentSound());
     }
 
